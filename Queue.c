6a000bcf938eca4970b961fc9192abbb4ee6752d
MrRightli <>
2017-11-21 18:00:02 +0800

    队列的基本算法

--- Queue.c
+++ Queue.c
@@ -16,7 +16,7 @@
  *              3. 循环队列各个参数的含义
  *                  1). 队列初始化
  *                          front 额值都是零
-*                   2). 队列非空
+ *                  2). 队列非空
  *                      front 代表的事队列的第一个元素
  *                      rear 代表的事队列的最后一个有效元素的下一位地址
  *                  3). 队列空
@@ -36,11 +36,110 @@
  *
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
-#import <stdbool.h>
+#include <stdbool.h>
+
+typedef struct Queue {
+    int *pBase;
+    int front;
+    int rear;
+} QUEUE;
+
+void init(QUEUE *);
+
+bool en_queue(QUEUE *, int);
+bool full_queue(QUEUE *);
+bool empty_queue(QUEUE *);
+bool out_queue(QUEUE *, int *);
+
+void traverse_queue(QUEUE *);
 
 int main(int argc, const char *argv[]) {
+    QUEUE Q;
 
+    init(&Q);
+    en_queue(&Q, 1);
+    en_queue(&Q, 2);
+    en_queue(&Q, 3);
+    en_queue(&Q, 4);
+
+    traverse_queue(&Q);
+
+    int outVal;
+    out_queue(&Q, &outVal);
+    printf("出队的数值为:%d\n",outVal);
+}
+
+void init(QUEUE *pQ) {
+    pQ->pBase = (int *) malloc(sizeof(int) * 6);
+    pQ->front = 0;
+    pQ->rear = 0;
+}
+
+/**
+ * 入队
+ * @param pQ
+ * @param val
+ * @return
+ */
+bool en_queue(QUEUE *pQ, int val) {
+    if (full_queue(pQ))
+        return false;
+
+    pQ->pBase[pQ->rear] = val;
+    pQ->rear = (pQ->rear + 1) % 6;
+}
+
+/**
+ * 出队
+ * @param pQ
+ * @param pVal
+ * @return
+ */
+bool out_queue(QUEUE *pQ, int * pVal) {
+    if (empty_queue(pQ))
+        return false;
+
+    *pVal = pQ->pBase[pQ->front];
+    pQ->front = (pQ->front + 1) % 6;
+
+    return true;
+}
+/**
+ * 队列是否满
+ * @param pQ
+ * @return
+ */
+bool full_queue(QUEUE *pQ) {
+    if (pQ->rear + 1 == pQ->front) {
+        return true;
+    } else {
+        return false;
+    }
+}
+/**
+ * 队列是否为空
+ * @param pQ
+ * @return
+ */
+bool empty_queue(QUEUE *pQ) {
+    if (pQ->rear% 6 == pQ->front) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * 遍历队列
+ * @param pQ
+ */
+void traverse_queue(QUEUE *pQ) {
+    int i = pQ->front;
+    while (i != pQ->rear) {
+        printf("%d ", pQ->pBase[i]);
+        i = (i + 1) % 6;
+    }
+    printf("\n");
 }
